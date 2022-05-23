d265b5ab996bd6c2e122ec2bdef04dd8f3834c2f
Kazemnezhad Mohamadreza <kazemnezhad_m@mapnamrm.local>
2022-05-23 09:37:51 +0430

    add post delete method

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ app_name='home'
 urlpatterns=[
     path('',views.HomeView.as_view(),name='home'),
     path('post/<int:post_id>/<slug:post_slug>/',views.PostView.as_view(),name='post'),
+    path('post/delete/<int:post_id>/',views.DeleteView.as_view(),name='delete_post'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 # Create your views here.
 
@@ -15,3 +17,18 @@ class PostView(View):
     def get(self,request,post_id,post_slug):
         post=Post.objects.get(pk=post_id,slug=post_slug)
         return render(request,'home/post.html',{'post':post})
+
+class DeleteView(LoginRequiredMixin,View):
+    def get(self,request,*arge,**kwargs):
+        post=Post.objects.get(pk=kwargs['post_id'])
+        if post.user.id==request.user.id:
+            post.delete()
+            messages.success(request,'Your post deleted successfully','success')
+        else:
+            messages.error(request,'Your cann\'t delete this post')
+        return redirect('home:home')
+    def post(self,request,*arge,**kwargs):
+        pass
+
+
+
