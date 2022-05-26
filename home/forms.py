6f5edfa2246fedc57b94990a761a1c2e7bd47f5f
mrkazemnejad <mreza.kazemnejad@gmail.com>
2022-05-26 15:13:35 +0430

    add reply to comment part

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Post
+from .models import Post,Comment
 
 
 class PostUpdateView(forms.ModelForm):
@@ -8,3 +8,11 @@ class PostUpdateView(forms.ModelForm):
         model=Post
         fields=('body',)
 
+
+class CommentFormView(forms.ModelForm):
+    class Meta:
+        model=Comment
+        fields=('body',)
+        widgets={
+            'body':forms.Textarea(attrs={'class':'form-control'})
+        }
--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from .models import Post,Relation
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostUpdateView
+from .forms import PostUpdateView,CommentFormView
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 
@@ -17,11 +17,25 @@ class HomeView(View):
 
 
 class PostView(View):
-    def get(self,request,post_id,post_slug):
+    form_class=CommentFormView
+    def setup(self,request,*args,**kwargs):
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['post_slug'])
+        return super().setup(request,*args,**kwargs)
+
+    def get(self,request,*args,**kwargs):
         # post=Post.objects.get(pk=post_id,slug=post_slug)
-        post=get_object_or_404(Post,pk=post_id,slug=post_slug)
-        comments=post.comment_post.filter(is_replay=False)
-        return render(request,'home/post.html',{'post':post,'comments':comments})
+        # post=get_object_or_404(Post,pk=post_id,slug=post_slug)
+        comments=self.post_instance.comment_post.filter(is_replay=False)
+        return render(request,'home/post.html',{'post':self.post_instance,'comments':comments,'form':self.form_class})
+    def post(self,request,*args,**kwargs):
+        form=self.form_class(request.POST)
+        if form.is_valid():
+            new_form=form.save(commit=False)
+            new_form.user=request.user
+            new_form.post=self.post_instance
+            new_form.save()
+            messages.success(request,'your comments save successfully','success')
+            return redirect('home:post',self.post_instance.id,self.post_instance.slug)
 
 class DeleteView(LoginRequiredMixin,View):
     def get(self,request,*arge,**kwargs):
