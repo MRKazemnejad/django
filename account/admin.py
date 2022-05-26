438e5567908c46b41c30c2533e9d9885cc38f16e
mrkazemnejad <mreza.kazemnejad@gmail.com>
2022-05-26 21:42:22 +0430

    change user model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import NewUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+class UserInline(admin.StackedInline):
+    model = NewUser
+
+class UserAdmin(BaseUserAdmin):
+    inlines=(UserInline,)
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class NewUser(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    age=models.PositiveSmallIntegerField(default=0)
+    title=models.CharField(max_length=100)
